76e13c39fe2eede3fa178b38326e4452c77eb9f9
Ed Summers <ehs@pobox.com>
2022-02-26 15:22:26 +0000

    added a sleep, and also records the batch run time

--- monitor.py
+++ monitor.py
@@ -1,27 +1,42 @@
 #!/usr/bin/env python3
 
 import csv
+import time
 import pathlib
 import datetime
 import requests
 import multiprocessing
+
+# collect data every 30 mins
+sleep_secs = 60 * 30
 
 def main():
     urls = open('urls.txt').read().splitlines()
 
     needs_header = not pathlib.Path('data.csv').is_file()
     fh = open('data.csv', 'a')
-    data = csv.DictWriter(fh, fieldnames=['time', 'url', 'error'])
+    data = csv.DictWriter(fh, fieldnames=['run', 'time', 'url', 'error'])
 
     if needs_header:
         data.writeheader()
 
     with multiprocessing.Pool(50) as pool:
         while True:
-            print(f'checking {len(urls)} urls')
+
+            started = datetime.datetime.now()
+            print(f'{started} checking {len(urls)} urls')
+
             for result in pool.map(check, urls):
                 if result:
+                    result['run'] = started.isoformat()
                     data.writerow(result)
+
+            # sleep for a bit if it took us less than sleep_secs to collect
+            elapsed = datetime.datetime.now() - started
+            if elapsed.total_seconds() < sleep_secs:
+                t = sleep_secs - elapsed.total_seconds()
+                print(f"sleeping {t} seconds")
+                time.sleep(t)
 
 def check(url):
     try:
