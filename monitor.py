1d1a160ec4b19da73fd29353bd69b0be76baca38
Ed Summers <ehs@pobox.com>
2022-02-26 14:10:15 +0000

    refactored to request urls in parallel

--- monitor.py
+++ monitor.py
@@ -1,23 +1,39 @@
 #!/usr/bin/env python3
 
 import csv
+import json
+import uuid
+import pathlib
 import datetime
 import requests
+import multiprocessing
 
-fh = open('errors.csv', 'a')
-data = csv.DictWriter(fh, fieldnames=['time', 'url', 'error'])
-data.writeheader()
+def main():
+    urls = open('urls.txt').read().splitlines()
 
-for url in open('urls.txt'):
-    url = url.strip()
+    needs_header = pathlib.Path('data.csv').is_file()
+    fh = open('data.csv', 'a')
+    data = csv.DictWriter(fh, fieldnames=['time', 'url', 'error'])
+
+    if needs_header:
+        data.writeheader()
+
+    with multiprocessing.Pool(50) as pool:
+        for result in pool.map(check, urls):
+            if result:
+                data.writerow(result)
+
+def check(url):
     try:
         resp = requests.get(url, timeout=30)
-        print(f'OK {url}')
+        return
     except Exception as e:
-        print(url, e)
-        data.writerow({
+        print(f"error: {url}")
+        return {
             "time": datetime.datetime.now().isoformat(),
             "url": url,
             "error": str(e)
-        })
-        fh.flush()
+        }
+
+if __name__ == "__main__":
+    main()
